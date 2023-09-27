829c3e913da7dc57e8d197d3c00315a7a913f5e8
kristampswang <oral.kristamps@gmail.com>
2023-09-26 18:56:26 -0700

    add more eslint style

--- packages/eslint-config-oceanjs/index.js
+++ packages/eslint-config-oceanjs/index.js
@@ -1,10 +1,17 @@
 module.exports = {
-    extends: [
-      './rules/imports',
-    ].map(require.resolve),
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    rules: {},
-  };
+  extends: [
+    './rules/best-practices',
+    './rules/errors',
+    './rules/node',
+    './rules/style',
+    './rules/variables',
+    './rules/es6',
+    './rules/imports',
+    './rules/strict',
+  ].map(require.resolve),
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+}
